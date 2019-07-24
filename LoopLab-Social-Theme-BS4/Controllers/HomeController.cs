02f183e673ad74e13393912676e08a2b03aa82bc
Sachin Limbu <sachinlimbu@outlook.com>
2019-07-24 17:39:29 +0100

    Add project files.

---
+++ LoopLab-Social-Theme-BS4/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LoopLab_Social_Theme_BS4.Models;
+
+namespace LoopLab_Social_Theme_BS4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LoopLab-Social-Theme-BS4/wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$('#year').text(new Date().getFullYear());
+
+$('body').scrollspy({ target: '#main-nav' });
+
+// Add smooth scrolling
+$('#main-nav a').on('click', function (e) {
+    // Check for a hash value
+    if (this.hash !== '') {
+        // Prevent default behavior
+        e.preventDefault();
+
+        // Store hash
+        const hash = this.hash;
+
+        // Animate smooth scroll
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 900, function () {
+            // Add hash to URL after scroll
+            window.location.hash = hash;
+        });
+    }
+});
